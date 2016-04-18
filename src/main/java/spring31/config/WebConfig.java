781ab89db5a6d8c7af19016c6f56d1a4595a2ef5
Young Jun Park <june0313@gmail.com>
2016-04-18 23:29:51 +0900

    [1.5.3] 웹 어플리케이션의 새로운 IoC 컨테이너 구성

---
+++ src/main/java/spring31/config/WebConfig.java
@@ -0,0 +1,10 @@
+package spring31.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wayne on 2016. 4. 18..
+ */
+@Configuration
+public class WebConfig {
+}
